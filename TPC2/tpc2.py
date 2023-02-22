50abbe68c90447ea21e2692a0e5b7e88b2b5d95d
myagi-ryota <a93285@alunos.uminho.pt>
2023-02-22 15:32:21 +0000

    OnOff implementado

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -5,10 +5,19 @@ while True:
     data = input("Escrever mensagem:\n")
     if 'Sair' == data:
         break
-    if onOff == 1:
-        lista = [int(s) for s in data.split() if s.isdigit()]
-        for n in lista:
-            soma += n
+    
+    words = data.split()
+    lista = []
+    for s in words:
+        s = s.upper()
+        if s == 'ON':
+            onOff = 1
+        if s == 'OFF':
+            onOff = 0
+        if s.isdigit() and onOff == 1:
+            lista.append(int(s))
+    for n in lista:
+        soma += n
 
 print(f'Soma = {soma}')
 print("Saída com sucesso")
